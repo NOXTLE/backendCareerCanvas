41810e06ec07c01c2ff2572646585c39de416ef0
Aditya Sharan <76609391+NOXTLE@users.noreply.github.com>
2025-09-27 09:10:22 +0530

    first commit

---
+++ main/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const router = require("./routes/auth");
+const resumeRouter = require("./routes/ResumeRoutes");
+const userRouter = require("./routes/userRoute");
+const jobController = require("./controllers/Scrape");
+const cors = require("cors");
+
+dotenv.config();
+const server = express();
+
+server.use(express.json());
+
+server.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+server.use("/auth", router);
+server.use("/resume", resumeRouter);
+server.use("/user", userRouter);
+server.use("/api", jobController);
+
+const main = async () => {
+  await mongoose.connect(process.env.URI);
+  console.log("MONGODB CONNECTED ");
+};
+
+server.listen(process.env.PORT, () => {
+  console.log("SERVER STARTED");
+  main().catch(() => {
+    console.log("ERROR IN DB");
+  });
+});
---
+++ main/routes/ResumeRoutes.js
@@ -0,0 +1,103 @@
+const express = require("express");
+const protect = require("../middlewares/protect");
+const resumeRouter = express.Router();
+const Resume = require("../model/Resume");
+
+resumeRouter
+  .post("/create", protect, async (req, res) => {
+    try {
+      const id = req.user._id;
+      req.body.userId = id;
+      const data = { userId: id, ...req.body };
+      const resume = new Resume(data);
+      const saved = await resume.save();
+      res.status(201).json({ message: "created", resume: saved });
+    } catch (err) {
+      res.status(500).json({ message: "server error" });
+    }
+  })
+  .get("/getAll/:page", protect, async (req, res) => {
+    try {
+      const user = req.user._id;
+      const page = parseInt(req.params.page);
+      const limit = 10;
+      const skip = (page - 1) * limit;
+      const userResumes = await Resume.find({ userId: user })
+        .sort({
+          lastUpdated: -1,
+        })
+        .skip(skip)
+        .limit(limit);
+      if (userResumes.length == 0)
+        return res.status(404).json({ message: "no resumes" });
+      console.log(userResumes);
+      res.status(200).json({ resumes: userResumes, count: userResumes.length });
+    } catch (err) {
+      res.status(500).json({ message: "server error" });
+    }
+  })
+  .get("/getOne/:id", protect, async (req, res) => {
+    try {
+      const user = req.user._id;
+      const resume = await Resume.findById(req.params.id);
+
+      if (!resume) {
+        return res.status(404).json({ message: "no resume found" });
+      }
+      if (!resume.userId.equals(req.user._id)) {
+        return res.status(403).json({
+          message: "unauthorized access ",
+        });
+      }
+
+      res.status(200).json({ resume: resume });
+    } catch (err) {
+      res.status(500).json({ message: "server error" });
+    }
+  })
+  .delete("/delete/:id", protect, async (req, res) => {
+    try {
+      const user = req.user._id;
+      const resume = await Resume.findById(req.params.id);
+
+      if (!resume) {
+        return res.status(404).json({ message: "no resume found" });
+      }
+      if (!resume.userId.equals(req.user._id)) {
+        return res.status(403).json({
+          message: "unauthorized access ",
+        });
+      }
+
+      await Resume.deleteOne({ _id: req.params.id });
+
+      res.status(200).json({ message: "resume deleted" });
+    } catch (err) {
+      res.status(500).json({ message: "server error" });
+    }
+  })
+  .put("/update/:id", protect, async (req, res) => {
+    try {
+      const id = req.user._id;
+      const data = { userId: id, ...req.body };
+      const resume = await Resume.findById(req.params.id);
+      if (!resume)
+        return res.status(404).json({ message: "No Resume found to update" });
+      if (!resume.userId.equals(req.user._id)) {
+        return res.status(403).json({
+          message: "unauthorized access ",
+        });
+      }
+      data.lastUpdated = Date.now();
+      const updated = await Resume.findOneAndUpdate(
+        { _id: resume._id },
+        { $set: data },
+        { new: true }
+      );
+      res.status(200).json({ message: "updated", resume: updated });
+    } catch (err) {
+      res.status(500).json({ message: "server error" });
+    }
+  });
+
+module.exports = resumeRouter;
